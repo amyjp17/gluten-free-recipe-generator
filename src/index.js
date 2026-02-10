8272ecf5eba09e1c0fc5e1b3040cf14dacf33601
amyjp17 <amypannell7@gmail.com>
2026-02-10 17:35:55 +0800

    add basic Javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: ["Hello"],
+    autoStart: true,
+    cursor: " ",
+  });
+}
+
+let recipeElement = document.querySelector("#recipe-generator-form");
+recipeElement.addEventListener("submit", generateRecipe);
