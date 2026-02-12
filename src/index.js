1ac77fa1b50a8b6255110db2c45a55d99e37bac3
amyjp17 <amypannell7@gmail.com>
2026-02-12 09:45:43 +0800

    improved UX

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 function displayRecipe(response) {
-  console.log("recipe generated");
   new Typewriter("#recipe", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 20,
+    delay: 5,
     cursor: " ",
   });
 }
 
 function generateRecipe(event) {
   event.preventDefault();
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.innerHTML = '<div class="blink"><em>Generating...</em></div>';
 
   let apiKey = "7296aa90736e3b3bc44c976ff5otbfe9";
   let instructionsInput = document.querySelector("#user-instructions");
@@ -18,11 +19,8 @@ function generateRecipe(event) {
     "You are an AI assistant that has a lot of knowledge about gluten free cooking. You will be given User Instructions that will either be ingredients or a recipe title and you will generate a recipe in basic HTML format including a title, ingredients list and method. Please use metric units. Please only include the HTML in your answer, not ```html. Use h2 for the recipe title and h3 for the ingredients and method headings.";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log(`Prompt: ${prompt}`);
-
   axios.get(apiUrl).then(displayRecipe);
-  console.log("generating recipe...");
 }
 
-let recipeElement = document.querySelector("#recipe-generator-form");
-recipeElement.addEventListener("submit", generateRecipe);
+let recipeForm = document.querySelector("#recipe-generator-form");
+recipeForm.addEventListener("submit", generateRecipe);
